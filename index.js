67e9080894310a9c7cd15a0c998636f9faec7207
Robin Naghshbandi <robinnaghshbandi12@gmail.com>
2023-06-20 14:31:35 +0100

    Refactored the internal functions

--- index.js
+++ index.js
@@ -2,94 +2,135 @@
 export const Phases = {
   // Function to apply sine wave effect to a div
   sineWave: (div, amplitude, frequency, phaseShift) => {
+    let transformValue = '';
     // Calculate the sine wave values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const sineValue = amplitude * Math.sin((i / frequency) + phaseShift);
-      // Apply the sine wave effect to the div
-      div.style.transform += `translateY(${sineValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${sineValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply cosine wave effect to a div
   cosineWave: (div, amplitude, frequency, phaseShift) => {
+    let transformValue = '';
     // Calculate the cosine wave values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const cosineValue = amplitude * Math.cos((i / frequency) + phaseShift);
-      // Apply the cosine wave effect to the div
-      div.style.transform += `translateY(${cosineValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${cosineValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
+    // Function to apply tangent wave effect to a div
+    tangentWave: (div, amplitude, frequency, phaseShift) => {
+      let transformValue = '';
+      // Calculate the tangent wave values for each pixel
+      for (let i = 0; i < div.offsetWidth; i++) {
+        const tangentValue = amplitude * Math.tan((i / frequency) + phaseShift);
+        // Accumulate the transform values
+        transformValue += `translateY(${tangentValue}px) `;
+      }
+      // Apply the accumulated transform value to the div
+      div.style.transform = transformValue;
+    },
+
+  // Function to apply linear function effect to a div
   linearFunction: (div, m, b) => {
+    let transformValue = '';
     // Calculate the linear function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const linearValue = m * i + b;
-      // Apply the linear function effect to the div
-      div.style.transform += `translateY(${linearValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${linearValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply quadratic function effect to a div
   quadraticFunction: (div, a, b, c) => {
+    let transformValue = '';
     // Calculate the quadratic function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const quadraticValue = a * Math.pow(i, 2) + b * i + c;
-      // Apply the quadratic function effect to the div
-      div.style.transform += `translateY(${quadraticValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${quadraticValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply cubic function effect to a div
   cubicFunction: (div, a, b, c, d) => {
+    let transformValue = '';
     // Calculate the cubic function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const cubicValue = a * Math.pow(i, 3) + b * Math.pow(i, 2) + c * i + d;
-      // Apply the cubic function effect to the div
-      div.style.transform += `translateY(${cubicValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${cubicValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply quadratic Bézier curve effect to a div
   quadraticBezierCurve: (div, p0, p1, p2) => {
+    let transformValue = '';
     // Calculate the quadratic Bézier curve values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const t = i / div.offsetWidth;
       const quadraticBezierValue = Math.pow(1 - t, 2) * p0 + 2 * (1 - t) * t * p1 + Math.pow(t, 2) * p2;
-      // Apply the quadratic Bézier curve effect to the div
-      div.style.transform += `translateY(${quadraticBezierValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${quadraticBezierValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply cubic Bézier curve effect to a div
   cubicBezierCurve: (div, p0, p1, p2, p3) => {
+    let transformValue = '';
     // Calculate the cubic Bézier curve values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const t = i / div.offsetWidth;
       const cubicBezierValue = Math.pow(1 - t, 3) * p0 + 3 * Math.pow(1 - t, 2) * t * p1 + 3 * (1 - t) * Math.pow(t, 2) * p2 + Math.pow(t, 3) * p3;
-      // Apply the cubic Bézier curve effect to the div
-      div.style.transform += `translateY(${cubicBezierValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${cubicBezierValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply circle function effect to a div
   circleFunction: (div, h, k, r) => {
+    let transformValue = '';
     // Calculate the circle function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const circleValue = Math.sqrt(Math.pow(r, 2) - Math.pow(i - h, 2)) + k;
-      // Apply the circle function effect to the div
-      div.style.transform += `translateY(${circleValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${circleValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   gaussianFunction: (div, amplitude, mean, standardDeviation) => {
+    let transformValue = '';
     for (let i = 0; i < div.offsetWidth; i++) {
       const x = i - div.offsetWidth / 2;
       const gaussianValue = amplitude * Math.exp(-(Math.pow(x - mean, 2) / (2 * Math.pow(standardDeviation, 2))));
-      div.style.transform += `translateY(${gaussianValue}px)`;
+      transformValue += `translateY(${gaussianValue}px) `;
     }
+    div.style.transform = transformValue;
   },
 
   piecewiseFunctions: (div, conditions) => {
+    let transformValue = '';
     for (let i = 0; i < div.offsetWidth; i++) {
       let value;
       for (const condition of conditions) {
@@ -99,57 +140,59 @@ export const Phases = {
         }
       }
       if (value !== undefined) {
-        div.style.transform += `translateY(${value}px)`;
+        transformValue += `translateY(${value}px) `;
       }
     }
+    div.style.transform = transformValue;
   },
 
   parametricEquations: (div, equations) => {
+    let transformValue = '';
     for (let i = 0; i < div.offsetWidth; i++) {
       const t = i / div.offsetWidth;
       const x = equations.x(t);
       const y = equations.y(t);
-      div.style.transform += `translate(${x}px, ${y}px)`;
+      transformValue += `translate(${x}px, ${y}px) `;
     }
+    div.style.transform = transformValue;
   },
 
   // Function to apply ellipse function effect to a div
   ellipseFunction: (div, h, k, a, b) => {
+    let transformValue = '';
     // Calculate the ellipse function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const ellipseValue = b * Math.sqrt(1 - Math.pow((i - h) / a, 2)) + k;
-      // Apply the ellipse function effect to the div
-      div.style.transform += `translateY(${ellipseValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${ellipseValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply hyperbola function effect to a div
   hyperbolaFunction: (div, h, k, a, b) => {
+    let transformValue = '';
     // Calculate the hyperbola function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const hyperbolaValue = b * Math.sqrt(Math.pow((i - h) / a, 2) - 1) + k;
-      // Apply the hyperbola function effect to the div
-      div.style.transform += `translateY(${hyperbolaValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${hyperbolaValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
 
   // Function to apply exponential function effect to a div
   exponentialFunction: (div, a, b) => {
+    let transformValue = '';
     // Calculate the exponential function values for each pixel
     for (let i = 0; i < div.offsetWidth; i++) {
       const exponentialValue = a * Math.exp(b * i);
-      // Apply the exponential function effect to the div
-      div.style.transform += `translateY(${exponentialValue}px)`;
+      // Accumulate the transform values
+      transformValue += `translateY(${exponentialValue}px) `;
     }
+    // Apply the accumulated transform value to the div
+    div.style.transform = transformValue;
   },
-
-  // Function to apply tangent wave effect to a div
-  tangentWave: (div, amplitude, frequency, phaseShift) => {
-    // Calculate the tangent wave values for each pixel
-    for (let i = 0; i < div.offsetWidth; i++) {
-      const tangentValue = amplitude * Math.tan((i / frequency) + phaseShift);
-      // Apply the tangent wave effect to the div
-      div.style.transform += `translateY(${tangentValue}px)`;
-    }
-  }
 };
--- phases.d.ts
+++ phases.d.ts
@@ -1,53 +1,22 @@
 declare module 'phases' {
-    // Import necessary modules
-    // import { MathType } from 'mathjs';
-  
-    // Define Phases object
-    export const Phases: {
-      // Function to apply sine wave effect to a div
-      sineWave: (div: HTMLDivElement, amplitude: number, frequency: number, phaseShift: number) => void;
-  
-      // Function to apply cosine wave effect to a div
-      cosineWave: (div: HTMLDivElement, amplitude: number, frequency: number, phaseShift: number) => void;
-  
-      // Function to apply linear function effect to a div
-      linearFunction: (div: HTMLDivElement, m: number, b: number) => void;
-  
-      // Function to apply quadratic function effect to a div
-      quadraticFunction: (div: HTMLDivElement, a: number, b: number, c: number) => void;
-  
-      // Function to apply cubic function effect to a div
-      cubicFunction: (div: HTMLDivElement, a: number, b: number, c: number, d: number) => void;
-  
-      // Function to apply quadratic Bézier curve effect to a div
-      quadraticBezierCurve: (div: HTMLDivElement, p0: number, p1: number, p2: number) => void;
-  
-      // Function to apply cubic Bézier curve effect to a div
-      cubicBezierCurve: (div: HTMLDivElement, p0: number, p1: number, p2: number, p3: number) => void;
-  
-      // Function to apply circle function effect to a div
-      circleFunction: (div: HTMLDivElement, h: number, k: number, r: number) => void;
-  
-      // Function to apply Gaussian function effect to a div
-      gaussianFunction: (div: HTMLDivElement, amplitude: number, mean: number, standardDeviation: number) => void;
-  
-      // Function to apply piecewise functions effect to a div
-      piecewiseFunctions: (div: HTMLDivElement, conditions: { start: number, end: number, equation: (x: number) => number }[]) => void;
-  
-      // Function to apply parametric equations effect to a div
-      parametricEquations: (div: HTMLDivElement, equations: { x: (t: number) => number, y: (t: number) => number }) => void;
-  
-      // Function to apply ellipse function effect to a div
-      ellipseFunction: (div: HTMLDivElement, h: number, k: number, a: number, b: number) => void;
-  
-      // Function to apply hyperbola function effect to a div
-      hyperbolaFunction: (div: HTMLDivElement, h: number, k: number, a: number, b: number) => void;
-  
-      // Function to apply exponential function effect to a div
-      exponentialFunction: (div: HTMLDivElement, a: number, b: number) => void;
-  
-      // Function to apply tangent wave effect to a div
-      tangentWave: (div: HTMLDivElement, amplitude: number, frequency: number, phaseShift: number) => void;
-    };
-  }
-  
+  export const Phases: {
+    sineWave: (div: HTMLDivElement, amplitude: number, frequency: number, phaseShift: number) => void;
+    cosineWave: (div: HTMLDivElement, amplitude: number, frequency: number, phaseShift: number) => void;
+    linearFunction: (div: HTMLDivElement, m: number, b: number) => void;
+    quadraticFunction: (div: HTMLDivElement, a: number, b: number, c: number) => void;
+    cubicFunction: (div: HTMLDivElement, a: number, b: number, c: number, d: number) => void;
+    quadraticBezierCurve: (div: HTMLDivElement, p0: number, p1: number, p2: number) => void;
+    cubicBezierCurve: (div: HTMLDivElement, p0: number, p1: number, p2: number, p3: number) => void;
+    circleFunction: (div: HTMLDivElement, h: number, k: number, r: number) => void;
+    gaussianFunction: (div: HTMLDivElement, amplitude: number, mean: number, standardDeviation: number) => void;
+    piecewiseFunctions: (
+      div: HTMLDivElement,
+      conditions: { start: number; end: number; equation: (x: number) => number }[]
+    ) => void;
+    parametricEquations: (div: HTMLDivElement, equations: { x: (t: number) => number; y: (t: number) => number }) => void;
+    ellipseFunction: (div: HTMLDivElement, h: number, k: number, a: number, b: number) => void;
+    hyperbolaFunction: (div: HTMLDivElement, h: number, k: number, a: number, b: number) => void;
+    exponentialFunction: (div: HTMLDivElement, a: number, b: number) => void;
+    tangentWave: (div: HTMLDivElement, amplitude: number, frequency: number, phaseShift: number) => void;
+  };
+}
